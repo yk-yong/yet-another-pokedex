3afe45335de871148755c995ee40de0efaee7b6e
John yk <yikok.yong@gmail.com>
2025-05-21 00:00:41 +0800

    fix: minor bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,63 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { SearchComponent } from './components/search/search.component';
-import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
-import { CommonModule } from '@angular/common';
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/shared/header/header.component";
+import { FooterComponent } from "./components/shared/footer/footer.component";
+import { SearchComponent } from "./components/search/search.component";
+import { PokemonCardComponent } from "./components/pokemon-card/pokemon-card.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [
-    RouterOutlet, 
+    RouterOutlet,
     CommonModule,
-    HeaderComponent, 
-    FooterComponent, 
+    HeaderComponent,
     SearchComponent,
     PokemonCardComponent,
-    NavigationComponent, 
-    FooterComponent
+    FooterComponent,
   ],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Pokédex Portfolio';
-  
+  title = "Pokédex Portfolio";
+
   // Sample Pokemon data for demonstration
   samplePokemon = [
-    { id: 1, name: 'bulbasaur', types: ['Grass', 'Poison'], imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png' },
-    { id: 4, name: 'charmander', types: ['Fire'], imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png' },
-    { id: 7, name: 'squirtle', types: ['Water'], imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png' },
-    { id: 25, name: 'pikachu', types: ['Electric'], imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png' }
+    {
+      id: 1,
+      name: "bulbasaur",
+      types: ["Grass", "Poison"],
+      imageUrl:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    },
+    {
+      id: 4,
+      name: "charmander",
+      types: ["Fire"],
+      imageUrl:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    },
+    {
+      id: 7,
+      name: "squirtle",
+      types: ["Water"],
+      imageUrl:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+    },
+    {
+      id: 25,
+      name: "pikachu",
+      types: ["Electric"],
+      imageUrl:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    },
   ];
-  
+
   // Handle search
   onSearch(searchTerm: string): void {
-    console.log('Searching for:', searchTerm);
+    console.log("Searching for:", searchTerm);
     // In a real app, this would filter the Pokemon list
   }
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,20 +1,56 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-pokemon-list',
+  selector: "app-pokemon-list",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.scss']
+  templateUrl: "./pokemon-list.component.html",
+  styleUrls: ["./pokemon-list.component.scss"],
 })
 export class PokemonListComponent {
   pokemonList = [
-    { id: 1, name: 'Bulbasaur', type: 'Grass/Poison', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png' },
-    { id: 4, name: 'Charmander', type: 'Fire', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png' },
-    { id: 7, name: 'Squirtle', type: 'Water', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png' },
-    { id: 25, name: 'Pikachu', type: 'Electric', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png' },
-    { id: 54, name: 'Psyduck', type: 'Water', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/054.png' },
-    { id: 94, name: 'Gengar', type: 'Ghost/Poison', imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/094.png' },
+    {
+      id: 1,
+      name: "Bulbasaur",
+      type: "Grass/Poison",
+      imageUrl:
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
+    },
+    {
+      id: 4,
+      name: "Charmander",
+      type: "Fire",
+      imageUrl:
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
+    },
+    {
+      id: 7,
+      name: "Squirtle",
+      type: "Water",
+      imageUrl:
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+    },
+    {
+      id: 25,
+      name: "Pikachu",
+      type: "Electric",
+      imageUrl:
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png",
+    },
+    {
+      id: 54,
+      name: "Psyduck",
+      type: "Water",
+      imageUrl:
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/054.png",
+    },
+    {
+      id: 94,
+      name: "Gengar",
+      type: "Ghost/Poison",
+      imageUrl:
+        "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/094.png",
+    },
   ];
 }
